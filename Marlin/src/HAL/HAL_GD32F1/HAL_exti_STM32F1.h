95ba79529fb09b664cd198428cb12a3dc6fc64a2
DLMCW <davidmcwhorter88@gmail.com>
2025-01-15 13:23:59 -0600

    Added HAL_GD32F1

---
+++ Marlin/src/HAL/HAL_GD32F1/HAL_exti_STM32F1.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// --------------------------------------------------------------------------
+// Includes
+// --------------------------------------------------------------------------
+
+#define PA 0
+#define PB 1
+#define PC 2
+#define PD 3
+#define PE 4
+#define PF 5
+#define PG 6
+
+typedef enum {
+    EXTI_Falling,
+    EXTI_Rising,
+    EXTI_Rising_and_falling
+}EXTI_MODE_E;
+/**
+* ExtiInit:Exti Interrup INIT_AUTO_FAN_PIN
+* para PortIndex:GPIOA-GPIOI
+* para PinIndex:0-15
+* para RisingFallingEdge:0-3,0:Falling, 1:Rising, 2:Rising and falling
+*/
+void ExtiInit(uint8_t PortIndex, uint8_t PinIndex, uint8_t RisingFallingEdge) ;
+void ExtiInit(uint8_t pin, EXTI_MODE_E mode) ;
+void EnableExtiInterrupt(uint8_t pin);
+void DisableExtiInterrupt(uint8_t pin);
+bool ExitGetITStatus(uint8_t pin);
+void ExtiClearITPendingBit(uint8_t pin);
